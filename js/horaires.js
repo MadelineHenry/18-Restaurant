4f8774813fd03a376590dd7bed00ec54bdd4f06a
Sifedine <Sifedinehajji01@gmail.com>
2020-12-08 12:07:41 +0100

    Menu Horaires Ouverture_Fermeture

---
+++ js/horaires.js
@@ -0,0 +1,44 @@
+document.querySelector('section').innerHTML = '';
+const section = document.querySelector('section');
+
+
+let weekday = [ 'Sun', 'Mon', 'Tue', 
+'Wed', 'Thu', 'Fri', 'Sat' ];
+
+const today = new Date()
+
+const contenaire = document.createElement('div')
+contenaire.className = 'contenaire_horaire'
+section.appendChild(contenaire)
+
+const Horaire = document.createElement('div');
+Horaire.id = 'heure'
+Horaire.className = 'Horaire_ouverture_fermeture';
+contenaire.appendChild(Horaire);
+
+const phrase_Ouvert_Fermer = document.createElement('div')
+    phrase_Ouvert_Fermer.className = 'phrase_Ouvert_Fermer'
+    contenaire.appendChild(phrase_Ouvert_Fermer)
+
+if (today.getHours() >= 12 && today.getHours() < 22 ){
+
+    phrase_Ouvert_Fermer.style = 'color:green'
+    phrase_Ouvert_Fermer.innerHTML +=  " (C'est ouvert) "
+
+}else {
+
+    phrase_Ouvert_Fermer.style = 'color: red;'
+    phrase_Ouvert_Fermer.innerHTML +=  " (C'est fermer) "
+}
+
+function UR_Start()
+{
+UR_Nu = new Date();
+UR_Indhold = showFilled(weekday[UR_Nu.getDay()]) + ' ' + showFilled(UR_Nu.toLocaleTimeString('en-UK'));
+Horaire.textContent = UR_Indhold;
+setTimeout("UR_Start()",1000);
+}
+function showFilled(Value)
+{
+return (Value > 9) ? "" + Value : "" + Value;
+}
